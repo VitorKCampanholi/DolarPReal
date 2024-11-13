db5f8b33bb305d5a97e279483e228d01645b1e01
VKBOOY <VKBOOY@DESKTOP-DE9CVF9>
2024-11-13 16:44:30 -0300

    Adicionar arquivos de projeto.

---
+++ ConversorMoeda/ConversorDeMoeda.cs
@@ -0,0 +1,11 @@
+﻿namespace ConversorMoeda
+{
+    internal class ConversorDeMoeda
+    {
+        public static double lof = 6.0;
+        public static double DolarParaReal(double quantia, double cotacao)  {
+            double total = quantia * cotacao;
+            return total + total * lof / 100.0;
+        }
+    }
+}
---
+++ ConversorMoeda/Program.cs
@@ -0,0 +1,13 @@
+﻿using ConversorMoeda;
+using System;
+using System.Globalization;
+
+Console.Write("Qual é a cotação do dólar? \n");
+double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.Write("Quantos Dólares você vai comprar:\n");
+double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+double result = ConversorDeMoeda.DolarParaReal(quantia, cotacao);
+
+Console.WriteLine("Valor a ser pago em reais = " + result.ToString("F2", CultureInfo.InvariantCulture));
